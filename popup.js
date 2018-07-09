2a6def489fa446681542b8e7ae9b52c84e7f2df6
m-teja <36612521+m-teja@users.noreply.github.com>
2018-07-08 17:07:08 -0700

    Added card drawing function

--- popup.js
+++ popup.js
@@ -29,6 +29,55 @@ function choose() {
             document.getElementById("test").innerHTML = "You rolled a " + rand;
             break;
 
+        case "Draw a random card":
+            var rand1 = Math.ceil((Math.random() * 4));
+            var suit;
+            switch (rand1) {
+                case 1:
+                    suit = "Hearts";
+                    break;
+
+                case 2:
+                    suit = "Diamonds";
+                    break;
+
+                case 3:
+                    suit = "Spades";
+                    break;
+
+                case 4:
+                    suit = "Clubs";
+                    break;
+                default:
+                    break;
+            }
+
+            var rand2 = Math.ceil((Math.random() * 13));
+            var rank;
+            switch (rand2) {
+                case 1:
+                    rank = "Ace";
+                    break;
+
+                case 11:
+                    rank = "Jack";
+                    break;
+
+                case 12:
+                    rank = "Queen";
+                    break;
+
+                case 13:
+                    rank = "King";
+                    break;
+                default:
+                    rank = rand2;
+                    break;
+            }
+            document.getElementById("test").innerHTML = rank + " of " + suit;
+            break;
+
+            
         case "Generate a number":
             document.getElementById("test").innerHTML = "rng";
             break;
